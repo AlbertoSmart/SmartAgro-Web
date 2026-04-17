012b4f771f9607850815fb97fb180c071f473e66
JaviRM11 <javirosamedina@gmail.com>
2026-04-17 11:35:29 +0200

    Solucionado al hacer clic en el apartado en blanco para que no se eliminen ni se quitan los cambios realizados

--- .astro/content.d.ts
+++ .astro/content.d.ts
@@ -108,17 +108,15 @@ declare module 'astro:content' {
 	>;
 
 	type ReturnTypeOrOriginal<T> = T extends (...args: any[]) => infer R ? R : T;
-	type InferEntrySchema<C extends keyof DataEntryMap>	type ExtractLoaderConfig<T> = T extends { loader: infer L } ? L : never;
+	type InferEntrySchema<C extends keyof DataEntryMap> = import('astro/zod').infer<
+		ReturnTypeOrOriginal<Required<ContentConfig['collections'][C]>['schema']>
+	>;
+	type ExtractLoaderConfig<T> = T extends { loader: infer L } ? L : never;
 	type InferLoaderSchema<
 		C extends keyof DataEntryMap,
 		L = ExtractLoaderConfig<ContentConfig['collections'][C]>,
 	> = L extends { schema: import('astro/zod').ZodSchema }
 		? import('astro/zod').infer<L['schema']>
-tro/zod').ZodSchema }
-		? import('astro/zod').infer<Required<ContentConfig['collections'][C]>['loader']['schema']>
-a']>
-a']>
-ma']>
 		: any;
 
 	type DataEntryMap = {
